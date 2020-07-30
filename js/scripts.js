aaf714bdbe875b0f3169dc52fb51b4c8ac4f241a
Nargiza Rysbek kyzy <R.nakosya@gmail.com>
2020-07-30 22:07:57 +0600

    slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,95 @@
+// first slide
 var images = [
-    "img/b1.jpg",
-    "img/b3.jpg",
-    "img/b2.jpg"
-];
+    "img/slide-1.png",
+    "img/b2.jpg",
+    "img/b1.jpg"
+    // "img/slide-3.png"
+]
 
-var prevBtn = $('#prev');
-var nextBtn = $('#next');
+var prev_btn = $('#prev');
+var next_btn = $('#next');
 var image = $('#image');
 
-var currentIndex = 0;
+var currentindex = 0;
 
-prevBtn.on('click', function () {
-    if (currentIndex > 0) {
-        currentIndex--;
-        // currentIndex = currentIndex - 1
+prev_btn.on('click', function () {
+    if (currentindex > 0) {
+        currentindex--;
     } else {
-        currentIndex = images.length - 1;
+        currentindex = images.length - 1;
     }
-    image.attr('src', images[currentIndex]);
+    image.attr('src', images[currentindex]);
 });
 
-nextBtn.on('click', function () {
-    if (currentIndex < images.length) {
-        currentIndex++;
-        // currentIndex = currentIndex + 1
+next_btn.on('click', function () {
+    if (currentindex < images.length - 1) {
+        currentindex++;
     } else {
-        currentIndex = 0;
+        currentindex = 0;
     }
-    image.attr('src', images[currentIndex]);
+    image.attr('src', images[currentindex]);
+});
+
+// middle slide
+var images_middle = [
+    "img/slide-2.png",
+    "img/b2.jpg",
+    "img/b1.jpg"
+]
+
+var prev_btn = $('#prev');
+var next_btn = $('#next');
+var middle_img = $('#middle-img');
+
+var middle_currentindex = 0;
+
+prev_btn.on('click', function () {
+    if (middle_currentindex > 0) {
+        middle_currentindex--;
+    } else {
+        middle_currentindex = images_middle.length - 1;
+    }
+    middle_img.attr('src', images_middle[middle_currentindex]);
+});
+
+next_btn.on('click', function () {
+    if (middle_currentindex < images.length - 1) {
+        middle_currentindex++;
+    } else {
+        middle_currentindex = 0;
+    }
+    middle_img.attr('src', images_middle[middle_currentindex]);
+});
+
+// last
+
+// middle slide
+var images_last = [
+    "img/slide-3.png",
+    "img/b2.jpg",
+    "img/b1.jpg"
+]
+
+var prev_btn = $('#prev');
+var next_btn = $('#next');
+var last_img = $('#last-img');
+
+var last_currentindex = 0;
+
+prev_btn.on('click', function () {
+    if (last_currentindex > 0) {
+        last_currentindex--;
+    } else {
+        last_currentindex = images_last.length - 1;
+    }
+    last_img.attr('src', images_last[last_currentindex]);
+});
+
+next_btn.on('click', function () {
+    if (last_currentindex < images_last.length - 1) {
+        last_currentindex++;
+    } else {
+        last_currentindex = 0;
+    }
+    last_img.attr('src', images_last[last_currentindex]);
 });
